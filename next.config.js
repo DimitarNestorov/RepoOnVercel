5eca5c9751eed6eb6ceb62a5ea4195493903b385
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2020-07-30 20:42:37 +0300

    Rewrites aren't experimental anymore 🥳

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-	experimental: {
-		async rewrites() {
-			return [
-				{ source: '/Release', destination: '/api/Release' },
-				{ source: '/./Release', destination: '/api/Release' },
-				{ source: '/./Packages', destination: '/api/Packages' },
-				{ source: '/./Packages.gz', destination: '/api/Packages.gz' },
-				{ source: '/Packages', destination: '/api/Packages' },
-				{ source: '/Packages.gz', destination: '/api/Packages.gz' },
-				{ source: '/./CydiaIcon.png', destination: '/CydiaIcon.png' },
-				{ source: '/./api/(.*)', destination: '/api/$1' },
-			]
-		},
+	async rewrites() {
+		return [
+			{ source: '/Release', destination: '/api/Release' },
+			{ source: '/./Release', destination: '/api/Release' },
+			{ source: '/./Packages', destination: '/api/Packages' },
+			{ source: '/./Packages.gz', destination: '/api/Packages.gz' },
+			{ source: '/Packages', destination: '/api/Packages' },
+			{ source: '/Packages.gz', destination: '/api/Packages.gz' },
+			{ source: '/./CydiaIcon.png', destination: '/CydiaIcon.png' },
+			{ source: '/./api/(.*)', destination: '/api/$1' },
+		]
 	},
 }
